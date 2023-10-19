59b8f1ff388d1be8b225075404518dffc5270024
Maycon Rodrigues <maycon.rf@outlook.com>
2023-10-18 23:00:14 -0300

    add atividade lookup

---
+++ JavaScript Algorithms and Data Structures/FundamentosJavaScript/generate-random-fractions-with-javascript.js
@@ -0,0 +1,11 @@
+function randomFraction() {
+
+  // Altere apenas o código abaixo desta linha
+let radom = Math.random();
+
+  return radom;
+
+  // Altere apenas o código acima desta linha
+}
+
+console.log(randomFraction())
---
+++ JavaScript Algorithms and Data Structures/FundamentosJavaScript/generate-random-whole-numbers-with-javascript.js
@@ -0,0 +1,10 @@
+function randomWholeNum() {
+
+  let radom = Math.floor(Math.random() * 10);
+
+  
+
+  return radom;
+}
+
+console.log(randomWholeNum())
